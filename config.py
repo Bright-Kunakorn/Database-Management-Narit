c91be06a6e4973e71e62159379829e15d92afaee
Kunakorn Topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2023-07-07 20:11:48 +0700

    Update config.py

--- config.py
+++ config.py
@@ -13,5 +13,4 @@ def config(filename='database.ini', section='postgresql'):
             db[param[0]] = param[1]
     else:
         raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
     return db
